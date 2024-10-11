e09719a7ccf85de1f6cb035c5baeec3abb439b14
Sam Seatt <samseatt@gmail.com>
2024-10-11 19:42:59 -0400

    task6

--- server.py
+++ server.py
@@ -3,15 +3,14 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/emotionDetector")
 def emot_detecter():
-    # text_to_analyze = request.args.get('textToAnalyze')
-
-    if "textToAnalyze=" in query_string:
-        text_to_analyze = query_string.split("textToAnalyze=")[-1]
-    else:
-        return "Error: textToAnalyze not found in request"
-
+    
+    text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     anger_score = response['anger']
@@ -24,11 +23,11 @@ def emot_detecter():
     result = (
         f"For the given statement, the system response is 'anger': {anger_score}, "
         f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and "
-        f"'sadness': {sadness_score}. The dominant emotion is '{dominant_emotion}'."
+        f"'sadness': {sadness_score}. The dominant emotion is <b>{dominant_emotion}</b>."
     )
 
     return result
 
 if __name__ == "__main__":
     #This functions executes the flask app and deploys it on localhost:5000
-    app.run(host="0.0.0.0", port=5003)
+    app.run(host="0.0.0.0", port=5000)
